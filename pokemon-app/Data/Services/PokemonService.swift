d16f788a67f6c3749c95df6db9b518555e32386d
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2022-10-01 17:00:56 +0700

    add header to service

--- pokemon-app/Data/Services/PokemonService.swift
+++ pokemon-app/Data/Services/PokemonService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RxSwift
+import Alamofire
 import RxAlamofire
 
 protocol PokemonService {
@@ -15,10 +16,11 @@ protocol PokemonService {
 
 class PokemonServiceImpl: PokemonService {
     private let baseURLString = "https://api.pokemontcg.io/v2/cards"
+    private let apiKey = "56d2f356-1213-42bd-99c4-1c8b14e1e3d7"
     private let scheduler = ConcurrentDispatchQueueScheduler(qos: DispatchQoS(qosClass: .background, relativePriority: 1))
     
     func fetchPokemonLists(query: String, page: Int) -> Observable<Result<PokemonPage, PokemonListError>> {
-        return RxAlamofire.request(.get, baseURLString, parameters: makeParameters(query: query, page: page))
+        return RxAlamofire.request(.get, baseURLString, parameters: makeParameters(query: query, page: page), headers: makeHeader())
             .responseData()
             .observe(on: scheduler)
             .expectingObject(ofType: NETPokemonResponse.self)
@@ -31,6 +33,7 @@ class PokemonServiceImpl: PokemonService {
                 
                 return Observable.just(newResult)
             }
+            .debug()
     }
     
     // MARK: - Private Func
@@ -44,5 +47,13 @@ class PokemonServiceImpl: PokemonService {
         ]
         
         return parameters
+    }
+    
+    private func makeHeader() -> HTTPHeaders {
+        let header: [String: String] = [
+            "X-Api-Key": apiKey
+        ]
+        
+        return HTTPHeaders(header)
     }
 }
